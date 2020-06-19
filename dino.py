a00fcbd5b1b4464371161b22658cb9b732ff15f3
Manasvi Sinha <46960182+sinha5432@users.noreply.github.com>
2020-06-19 22:32:11 +0530

    Add files via upload

--- dino.py
+++ dino.py
@@ -5,6 +5,7 @@ import cv2
 import pyautogui
 import webbrowser
 import time
+import math
 
 def screen_record():
     print('Entered')
@@ -12,38 +13,35 @@ def screen_record():
     while True:
         elapsed_time = time.time()-last_time
 
-        left_b = 670 + 1.5*elapsed_time
-        right_b = 814 + 1.5*elapsed_time
+        left_b = 724 + int(1.2*math.sqrt(elapsed_time))
+        right_b = 825 + int(1.6*math.sqrt(elapsed_time))
 
         if(left_b>1178):
             left_b = 1178
         if(right_b>1322):
             right_b = 1322
 
-        printscreen_pil =  ImageGrab.grab(bbox = (left_b,332,right_b,380))
+        printscreen_pil =  ImageGrab.grab(bbox = (left_b,330,right_b,383))
         printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
-        gray_screen = cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2GRAY)
 
-        l,b = np.shape(gray_screen);
+        gray_screen = cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2GRAY)
+        img = cv2.Canny(gray_screen, 20, 250)
+        kernel = np.ones((5, 5), np.uint8)
+        img = cv2.dilate(img, kernel, iterations=4)
+
+        l,b = np.shape(img);
         middle = int((l)/2)
         upper_box = gray_screen[:middle,:]
         lower_box = gray_screen[middle:,:]
 
-        lower_avg = np.sum(lower_box)/(2*l*b)
+        lower_avg = np.sum(lower_box)/(l*b)
         print(lower_avg)
 
-        '''cv2.imshow('window',cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2GRAY))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-        '''
         if(keyboard.is_pressed('alt')):
             print('exiting')
             break
-        elif(lower_avg<60.02):
+        elif(lower_avg<120):
             pyautogui.typewrite(["up"])
-
-
 
 
 
@@ -51,7 +49,7 @@ def screen_record():
 if __name__=='__main__':
     webbrowser.open('https://chromedino.com/', new=0, autoraise=True)
     while True:
-        if(keyboard.is_pressed('ctrl')):
+        if(keyboard.is_pressed('space')):
             screen_record()
             break
 
